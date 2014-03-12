98766783d1c33aa89fea59834c23d615c8737fa5
zernovagg <zernovagg@gmail.com>
2014-03-12 16:17:00 +0200

    wolf walks for static rabbit
    without checking close positions

--- app/finder.js
+++ app/finder.js
@@ -10,12 +10,16 @@
 var pfinder = {
     'size'  : {},
     'start' : {},
+	'startR':{},
     'end'   : {},
+	'endR':{},
     'distance' : 0,
     'open' : {},
     'close' : {},
     'curr' : {},
+	'currR':{},
     'min' : 0,
+	'max' : 0,
 
     genArea : function(pos_x, pos_y){
         pfinder.size = {x:pos_x, y:pos_y};
@@ -52,23 +56,41 @@ var pfinder = {
 
     setStart : function(pos_x, pos_y){
         pfinder.start = {x:pos_x, y:pos_y};
-        $('#pos_'+pos_x+'_'+pos_y).addClass('curr');
+        $('#pos_'+pos_x+'_'+pos_y).addClass('currWolf');
         pfinder.curr = pfinder.start;
     },
-
-    setEnd : function(pos_x, pos_y){
-        pfinder.end = {x:pos_x, y:pos_y};
-        $('#pos_'+pos_x+'_'+pos_y).addClass('end');
+	
+	setStartR : function(pos_x, pos_y){
+        pfinder.startR = {x:pos_x, y:pos_y};
+        $('#pos_'+pos_x+'_'+pos_y).addClass('currRabbit');
+        pfinder.currR = pfinder.startR;
     },
 
+    setEnd : function(){
+        pfinder.end = {x:pfinder.currR.x, y:pfinder.currR.y};
+       
+    },
+
+	setEndR : function(){
+        pfinder.endR = {x:pfinder.curr.x, y:pfinder.curr.y};
+       
+    },
+	
     calcDistance : function(start_x, start_y, end_x, end_y){
         return Math.abs(end_x - start_x) + Math.abs(end_y - start_y);
     },
 
     setPos : function(pos_x, pos_y){
-        $('#area div.curr').removeClass('curr');
+        $('#area div.currWolf').removeClass('currWolf');
 
-        $('#pos_'+pos_x+'_'+pos_y).addClass('curr');
+        $('#pos_'+pos_x+'_'+pos_y).addClass('currWolf');
+        pfinder.curr = {x:pos_x, y:pos_y};
+    },
+	
+	setPosR : function(pos_x, pos_y){
+        $('#area div.currRabbit').removeClass('currRabbit');
+
+        $('#pos_'+pos_x+'_'+pos_y).addClass('currRabbit');
         pfinder.curr = {x:pos_x, y:pos_y};
     },
 
@@ -112,14 +134,39 @@ var pfinder = {
         if(!define){
             if(pfinder.open[pos_x] == undefined)
                 pfinder.open[pos_x] = [];
-            pfinder.open[pos_x][pos_y] = {x:parent_x,
-                                          y:parent_y,
-                                          to_end: pfinder.calcDistance(pos_x, pos_y, pfinder.end.x, pfinder.end.y),
-                                          to_start: pfinder.calcDistance(pos_x, pos_y, pfinder.start.x, pfinder.start.y)
+            pfinder.open[pos_x][pos_y] = {
+                                          to_end: pfinder.calcDistance(pos_x, pos_y, pfinder.end.x, pfinder.end.y)
+                                          
                                         };
             $('#pos_'+pos_x+'_'+pos_y).addClass('open');
         }
     },
+	
+	addOpenR : function(pos_x, pos_y, parent_x, parent_y){
+        var define = false;
+        if(pfinder.open[pos_x] != undefined){
+            if(pfinder.open[pos_x][pos_y] != undefined){
+                var define = true;
+            }
+        }
+
+        if(pfinder.close[pos_x] != undefined){
+            if(pfinder.close[pos_x][pos_y] != undefined){
+                var define = true;
+            }
+        }
+
+        if(!define){
+            if(pfinder.open[pos_x] == undefined)
+                pfinder.open[pos_x] = [];
+            pfinder.open[pos_x][pos_y] = {
+                                          
+                                          to_endR: pfinder.calcDistance(pos_x, pos_y, pfinder.endR.x, pfinder.endR.y)
+                                        };
+            $('#pos_'+pos_x+'_'+pos_y).addClass('open');
+        }
+    },
+	
 	
 	 addOpenLight : function(pos_x, pos_y){
         var define = false;
@@ -211,7 +258,9 @@ var pfinder = {
                 }
             }
         }
-		 if(pfinder.min) pfinder.setPos(pfinder.min.x, pfinder.min.y);
+		 if(pfinder.min) { pfinder.setPos(pfinder.min.x, pfinder.min.y);
+		 pfinder.setEndR();
+		 }
 		 
 	/*	 for (var y = min_y; y <= max_y; y++){
             for (var x = min_x; x <= max_x; x++){
@@ -223,6 +272,55 @@ var pfinder = {
         };*/
 		 
     },
+	
+	
+    findAroundR : function(){
+
+       
+
+        pfinder.addClose(pfinder.currR.x, pfinder.currR.y);
+
+        var min_x = eval(pfinder.currR.x) - 1;
+        var min_y = eval(pfinder.currR.y) - 1;
+        var max_x = eval(pfinder.currR.x) + 1;
+        var max_y = eval(pfinder.currR.y) + 1;
+
+        for (var y = min_y; y <= max_y; y++){
+            for (var x = min_x; x <= max_x; x++){
+                if(x > 0 && y > 0 && x <= pfinder.size.x && y <= pfinder.size.y)
+                    pfinder.addOpenR(x, y, pfinder.currR.x, pfinder.currR.y);
+            };
+        };
+
+		pfinder.max = 0;
+        if(Object.keys(pfinder.open).length){
+            for(var pos_x in pfinder.open){
+                if(pfinder.open[pos_x] != undefined){
+                    for(var pos_y in pfinder.open[pos_x]){
+                        if(pfinder.open[pos_x][pos_y] != undefined){
+                            var f = pfinder.open[pos_x][pos_y].to_endR;
+                          //  if(pfinder.curr.x != pos_x && pfinder.curr.y != pos_y) f = f * 1.5;
+                            $('#pos_'+pos_x+'_'+pos_y).text(f);
+                            if(pfinder.max){
+                                if(f > pfinder.max.amount 
+								//&& pfinder.calcDistance(pos_x, pos_y, pfinder.curr.x, pfinder.curr.y) <= 1
+								){
+                                    pfinder.max = {x:pos_x, y:pos_y, amount:f};
+                                }
+                            }else{
+                                pfinder.max = {x:pos_x, y:pos_y, amount:f};
+                            }
+                        }
+                    }
+                }
+            }
+        }
+		  if(pfinder.max) { pfinder.setPosR(pfinder.max.x, pfinder.max.y);
+		 pfinder.setEnd();
+		 }
+		 
+	    },
+	
 	
     find : function(swolf){
         //pfinder.distance = pfinder.calcDistance(pfinder.start.x, pfinder.start.y, pfinder.end.x, pfinder.end.y);
@@ -255,9 +353,36 @@ var pfinder = {
         }
 		
        
+    },
+	
+    findR : function(sRabbit){
+      
+        pfinder.addOpen(pfinder.currR.x, pfinder.currR.y, pfinder.currR.x, pfinder.currR.y);
+
+       
+		var sRabbit = sRabbit;
+		for (var count = 1; count<=sRabbit; count++){setTimeout(finderR(), 0)};
+        function finderR(){
+            pfinder.findAroundR();
+			//pfinder.setStartR (pfinder.currR.x, pfinder.currR.y);
+            console.log(pfinder);
+		
+									
+								
+            if(pfinder.currR.x == pfinder.endR.x && pfinder.currR.y == pfinder.endR.y) alert("Game over!");
+			
+        }
+		
+       
     }
+	
+	
 };
 
-function wolf(speed, swolf){
-pfinder.find.call(this, speed, swolf);
+function wolf(swolf){
+pfinder.find.call(this, swolf);
+};
+
+function rabbit(sRabbit){
+pfinder.findR.call(this, sRabbit);
 };
